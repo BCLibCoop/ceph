b5fa66fdf8ab6968dea06ae4ceb67f90e277df52
Sage Weil <sage@inktank.com>
2013-12-13 13:11:27 -0800

    osd: rename IGNORE_OVERLAY -> IGNORE_CACHE
    This is about skipping cache logic, not the tier pool overlay property.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/test/librados/tier.cc
+++ src/test/librados/tier.cc
@@ -318,7 +318,7 @@ TEST(LibRadosTier, Evict) {
     op.cache_evict();
     librados::AioCompletion *completion = cluster.aio_create_completion();
     ASSERT_EQ(0, cache_ioctx.aio_operate(
-      "foo", completion, &op, librados::OPERATION_IGNORE_OVERLAY));
+      "foo", completion, &op, librados::OPERATION_IGNORE_CACHE));
     completion->wait_for_safe();
     ASSERT_EQ(0, completion->get_return_value());
     completion->release();
@@ -329,7 +329,7 @@ TEST(LibRadosTier, Evict) {
     librados::AioCompletion *completion = cluster.aio_create_completion();
     ASSERT_EQ(0, cache_ioctx.aio_operate(
       "fooberdoodle", completion, &op,
-      librados::OPERATION_IGNORE_OVERLAY));
+      librados::OPERATION_IGNORE_CACHE));
     completion->wait_for_safe();
     ASSERT_EQ(-ENOENT, completion->get_return_value());
     completion->release();
@@ -340,7 +340,7 @@ TEST(LibRadosTier, Evict) {
     librados::AioCompletion *completion = cluster.aio_create_completion();
     ASSERT_EQ(0, cache_ioctx.aio_operate(
       "bar", completion, &op,
-      librados::OPERATION_IGNORE_OVERLAY));
+      librados::OPERATION_IGNORE_CACHE));
     completion->wait_for_safe();
     ASSERT_EQ(-EBUSY, completion->get_return_value());
     completion->release();
